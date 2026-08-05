005cdc2754f6957e4663d7f78e177142f501565f
Gibo Blayze <giboblayze0@gmail.com>
2026-08-05 20:13:10 +0200

    Create manage.php

---
+++ admin/manage.php
@@ -0,0 +1,17 @@
+<td>
+
+<a href="edit.php?id=<?php echo $song['id']; ?>">
+Edit
+</a>
+
+
+|
+
+<a href="delete.php?id=<?php echo $song['id']; ?>"
+onclick="return confirm('Delete this song?')">
+
+Delete
+
+</a>
+
+</td>
